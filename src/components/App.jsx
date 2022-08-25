bd6bc65a7ad199073ae7f3af5769ae12931f1e2c
heyulan0112 <heyulan0112@gmail.com>
2022-08-25 18:57:37 -0400

    update add and delete function

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,23 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
-import notes from "../notes";
+import CreateArea from "./CreateArea";
 
+function App() {
+    const [notes, setNotes] = useState([]);
+    function AddNote(newNote){
+        return setNotes(preValue => {
+            return [
+                ...preValue,
+                newNote
+            ]
+        });
+    }
 
-function App(){
-    console.log(notes);
-    return <div>
+    function DeleteNote(id){
+        setNotes(preValue => {
+            return preValue.filter( (n,index) => {
+                    return index !== id;
+            } )
+        });
+    }
+
+    return (
+        <div>
         <Header />
-        {notes.map( (noteTerm) => ( 
-        <Note
-            key = {noteTerm.key}
-            title = {noteTerm.title}
-            content = {noteTerm.content}
+        <CreateArea 
+            onAdd = {AddNote}
         />
-        ))}
+        {notes.map((noteItem,index) => {
+            return <Note 
+                key={index}  
+                id={index} 
+                title={noteItem.title} 
+                content={noteItem.content}
+                onDelete = {(id) => {
+                    DeleteNote(id)
+                }}
+            />
+        })}
+        {/* <Note key={1} title="Note title" content="Note content" /> */}
         <Footer />
-    </div>
+        </div>
+  );
 }
 
 export default App;
---
+++ src/components/CreateArea.jsx
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+
+function CreateArea(props) {
+    const [inputText, setInputText] = useState({
+        title: "",
+        content: ""
+    });
+    function hancleChange(event){
+        const {name,value} = event.target;
+        return setInputText(preValue => {
+            return {
+                ...preValue,
+                [name]:value
+            }
+        });
+    }
+
+    function handleAdd(event){
+        props.onAdd(inputText);
+        setInputText({
+            title: "",
+            content: ""
+        });
+        event.preventDefault();
+    }
+
+    return (
+        <div>
+        <form>
+            <input 
+            onChange={hancleChange} 
+            value={inputText.title} name="title" 
+            placeholder="Title" />
+            <textarea 
+            onChange={hancleChange} 
+            value={inputText.content} name="content" 
+            placeholder="Take a note..." 
+            rows="3" />
+            <button onClick={handleAdd}>Add</button>
+        </form>
+        </div>
+  );
+}
+
+export default CreateArea;
